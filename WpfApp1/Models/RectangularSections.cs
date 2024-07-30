1123c225d14e832a63432492264aed6d7b52fe72
Riddhi Goswami <91322531+Secret-Ambush@users.noreply.github.com>
2024-07-30 12:40:48 +0400

    Update RectangularSections.cs

--- WpfApp1/Models/RectangularSections.cs
+++ WpfApp1/Models/RectangularSections.cs
@@ -11,19 +11,12 @@ namespace WpfApp1
     {
         public override double[] RebarInertiaCal(double width, double height, double StirrupThickness, ObservableCollection<Rebars> userInputtedRebars, double cover, double sidecover)
         {
-            double rebarIx = 0;
-            double rebarIy = 0;
-            double totalAreaOfRebars = 0;
-            int countOfOfRebarsInTheWholeSection = 0;
 
-            foreach (var item in userInputtedRebars)
-            {
-                countOfOfRebarsInTheWholeSection += item.NumOfRebar;
-            }
+            int countOfOfRebarsInTheWholeSection = GetTotalNumberOfRebars(userInputtedRebars);
 
             Coordinates[] rebarsCoordinates = new Coordinates[countOfOfRebarsInTheWholeSection];
 
-            if (width != 0 && height !=0 && userInputtedRebars is not null)
+            if (width != 0 && height != 0 && userInputtedRebars is not null)
             {
                 int j = 0;
                 foreach (var item in userInputtedRebars)
@@ -32,7 +25,7 @@ namespace WpfApp1
                     double spaceForRebarPlacement = width - 2 * (sidecover + StirrupThickness);
                     double spacing = (spaceForRebarPlacement - 2 * rebarRadius) / (item.NumOfRebar - 1);
                     double areaOfRebar = CalcRebarArea(item.RebarDia);
-                    
+
                     for (int i = 0; i < item.NumOfRebar; i++)
                     {
                         double x = sidecover + StirrupThickness + (i * spacing);
@@ -47,16 +40,8 @@ namespace WpfApp1
 
                 }
 
-                for (int i = 0; i < countOfOfRebarsInTheWholeSection - 1; i++)
-                {
-                    double diff_x = rebarsCoordinates[i].x_coordinate - rebarsCoordinates[i + 1].x_coordinate;
-                    double diff_y = rebarsCoordinates[i].y_coordinate - rebarsCoordinates[i + 1].y_coordinate;
-                    double sum_radius = rebarsCoordinates[i].radius + rebarsCoordinates[i + 1].radius;
-                    if ((Math.Pow(diff_x, 2) + Math.Pow(diff_y, 2)) < Math.Pow(sum_radius, 2))
-                    {
-                        return [-1, -1, -1];
-                    }
-                }
+                if (AreThereOverlappingRebars(rebarsCoordinates, countOfOfRebarsInTheWholeSection))
+                    return [-1, -1, -1];
 
             }
 
@@ -68,7 +53,7 @@ namespace WpfApp1
             double totalIx = (breadth * Math.Pow(height, 3)) / 12;
             double totalIy = (breadth * Math.Pow(height, 3)) / 12;
 
-            return [Math.Round(totalIx, 6), Math.Round(totalIy, 6) ];
+            return [Math.Round(totalIx, 6), Math.Round(totalIy, 6)];
         }
     }
 }
