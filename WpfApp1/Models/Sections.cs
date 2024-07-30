500b73f4e7d7afbb7064f9ea17a9e6c34addecaa
Riddhi Goswami <91322531+Secret-Ambush@users.noreply.github.com>
2024-07-30 12:40:32 +0400

    Update Sections.cs

--- WpfApp1/Models/Sections.cs
+++ WpfApp1/Models/Sections.cs
@@ -12,7 +12,22 @@ namespace WpfApp1
 
         protected const double PI = 3.14;
 
-        public double CalcRebarArea( double diameterOfRebar) 
+        protected double rebarIx = 0;
+        protected double rebarIy = 0;
+        protected double totalAreaOfRebars = 0;
+
+        public int GetTotalNumberOfRebars(ObservableCollection<Rebars> userInputtedRebars)
+        {
+            int count = 0;
+            foreach (var item in userInputtedRebars)
+            {
+                count += item.NumOfRebar;
+            }
+
+            return count;
+        }
+        
+        public double CalcRebarArea(double diameterOfRebar)
         {
 
             double radius_Rebar = diameterOfRebar / 2;
@@ -23,22 +38,38 @@ namespace WpfApp1
 
         public virtual double[] RebarInertiaCal(double radiusOfRebar, double StirrupThickness, ObservableCollection<Rebars> userInputtedRebars, double cover)
         {
-            return new double[0];
+            return [];
         }
 
         public virtual double[] RebarInertiaCal(double width, double height, double StirrupThickness, ObservableCollection<Rebars> userInputtedRebars, double cover, double sidecover)
         {
-            return new double[0];
+            return [];
         }
 
         public virtual double[] TotalInertiaCal(double diameterOfColumn)
         {
-            return new double[0];
+            return [];
         }
 
         public virtual double[] TotalInertiaCal(double height, double width)
         {
-            return new double[0];
-        } 
+            return [];
+        }
+
+        public bool AreThereOverlappingRebars(Coordinates[] rebarCoordintes, int total)
+        {
+            for (int i = 0; i < total - 1; i++)
+            {
+                double diff_x = rebarCoordintes[i].x_coordinate - rebarCoordintes[i + 1].x_coordinate;
+                double diff_y = rebarCoordintes[i].y_coordinate - rebarCoordintes[i + 1].y_coordinate;
+                double sum_radius = rebarCoordintes[i].radius + rebarCoordintes[i + 1].radius;
+                if ((Math.Pow(diff_x, 2) + Math.Pow(diff_y, 2)) < Math.Pow(sum_radius, 2))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
     }
 }
