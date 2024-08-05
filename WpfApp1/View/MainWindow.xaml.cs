4d991040c0f668932915dd39a222f6024e1f8ad2
Riddhi Goswami <91322531+Secret-Ambush@users.noreply.github.com>
2024-08-05 09:00:12 +0400

    Update MainWindow.xaml.cs

--- WpfApp1/View/MainWindow.xaml.cs
+++ WpfApp1/View/MainWindow.xaml.cs
@@ -1,29 +1,13 @@
-﻿using System.Collections.ObjectModel;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Input;
-using System.Windows.Media;
+using Jacobs.Orchestra.UI.Windows;
 
 namespace WpfApp1
 {
-    public partial class MainWindow : Window
+    public partial class MainWindow : ThemedWindow
     {
-
         public MainWindow()
         {
             InitializeComponent();
             DataContext = new WindowViewModel(this);
-        }
-
-        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            if (e.ChangedButton == MouseButton.Left)
-                DragMove();
-        }
-
-        private void section_Loaded(object sender, RoutedEventArgs e)
-        {
-            section.SelectedIndex = 1;
         }
     }
 }
