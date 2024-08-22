a949741db8ad84db51d5e74b95a7313d38c47e9c
Riddhi Goswami <91322531+Secret-Ambush@users.noreply.github.com>
2024-08-22 08:52:44 +0400

    Update ReportWindow.xaml.cs

--- WpfApp1/View/ReportWindow.xaml.cs
+++ WpfApp1/View/ReportWindow.xaml.cs
@@ -1,9 +1,6 @@
 using MahApps.Metro.Controls;
-using Microsoft.Win32;
 using System.IO;
-using System.IO.Packaging;
 using System.Windows;
-using System.Windows.Documents;
 using System.Windows.Media;
 using System.Windows.Xps.Packaging;
 
@@ -13,7 +10,7 @@ namespace WpfApp1
     {
         public DocumentConverter _documentConverter;
         private string tempFilePath;
-        List<Visual> visual = null;
+        List<Visual> visualsList = null;
         byte[] pdfData = null;
 
         public ReportWindow(List<Visual> MainWindowVisuals)
@@ -21,9 +18,9 @@ namespace WpfApp1
             InitializeComponent();
 
             _documentConverter = new DocumentConverter();
-            visual = MainWindowVisuals;
+            visualsList = MainWindowVisuals;
             var pageSize = new Size(800, 1000);
-            var paginator = new MyDocumentPaginator(visual, pageSize, false);
+            var paginator = new MyDocumentPaginator(visualsList, pageSize, false);
             pdfData = _documentConverter.ConvertToPdf(paginator, pageSize);
 
             if (pdfData != null)
@@ -35,46 +32,6 @@ namespace WpfApp1
                 MessageBox.Show("Failed to load byte array.");
             }
 
-        }
-
-        private FixedDocumentSequence GetFixedDocumentSequence(byte[] xpsBytes)
-        {
-            Uri packageUri;
-            XpsDocument xpsDocument = null;
-
-            using (MemoryStream xpsStream = new MemoryStream(xpsBytes))
-            {
-                using (Package package = Package.Open(xpsStream))
-                {
-                    packageUri = new Uri("xpsStream://myXps.xps");
-                    PackageStore.AddPackage(packageUri, package);
-
-                    try
-                    {
-                        xpsDocument = new XpsDocument(package, CompressionOption.Maximum, packageUri.AbsoluteUri);
-                        return xpsDocument.GetFixedDocumentSequence();
-                    }
-                    finally
-                    {
-                        xpsDocument?.Close();
-                        PackageStore.RemovePackage(packageUri);
-                    }
-                }
-            }
-        }
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            // Save the PDF data
-            SaveFileDialog saveFileDialog = new SaveFileDialog
-            {
-                Filter = "PDF files (*.pdf)|*.pdf",
-                DefaultExt = "pdf"
-            };
-
-            if (saveFileDialog.ShowDialog() == true)
-            {
-                File.WriteAllBytes(saveFileDialog.FileName, pdfData);
-            }
         }
         private void DisplayXpsDocument(byte[] byteArray)
         {
